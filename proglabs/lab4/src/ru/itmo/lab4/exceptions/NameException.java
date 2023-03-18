2a9652e1583266592d421ee25f78c9a6d33739b2
Taisiia Minakova <116767207+taisssam@users.noreply.github.com>
2023-03-18 09:37:37 +0300

    Delete proglabs/lab4 directory

--- proglabs/lab4/src/ru/itmo/lab4/exceptions/NameException.java
+++
@@ -1,8 +0,0 @@
-package ru.itmo.lab4.exceptions;
-
-//lab4 unchecked_exception
-public class NameException extends RuntimeException{
-    public NameException(String message){
-        super(message);
-    }
-}
